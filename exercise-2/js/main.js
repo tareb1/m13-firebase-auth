5343b23ccc6ff4924f92d6e0abff8403333366e1
Admin <rebeccata@D-173-250-246-29.dhcp4.washington.edu>
2016-10-25 14:38:54 -0700

    participation

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -1,15 +1,16 @@
 $(function() {
 	// Set the config object with the values for your application
 	var config = {
-	    apiKey: "",
-	    authDomain: "",
-	    databaseURL: "",
-	    storageBucket: "",
-	    messagingSenderId: ""
+		apiKey: "AIzaSyCR1jnU6mApIaIzLUIXcxXnrVewhvSbW_E",
+		authDomain: "awesome-stuff-18c59.firebaseapp.com",
+		databaseURL: "https://awesome-stuff-18c59.firebaseio.com",
+		storageBucket: "awesome-stuff-18c59.appspot.com",
+		messagingSenderId: "731170506996"
 	};
 	firebase.initializeApp(config);
 
 	// Initialize your 'users' firebase reference
+	var userRef = firebase.database().ref("users");
 
 	// Checks to see if a user is already signed in and if so redirects
 	// to users.html
@@ -23,20 +24,39 @@ $(function() {
 		// provider accordingly (more info on login with 3rd party
 		// providers can be found in the firebase documentation).
 
-		// Sign into firebase using the correct provider
+		var provider;
+		if (service === "google") {
+			provider = new firebase.auth.GoogleAuthProvider();
+		} else if (service === "github") {
+			provider = new firebase.auth.GithubAuthProvider();
+		}
 
+
+		// Sign into firebase using the correct provider
+			firebase.auth().signInWithPopup(provider).then(function(result) {
 			// Log the user result to the console to see what a user object
 			// looks like
+					console.log(result);
 
+					// Check to see if we have already saved a user with this id
+					// to our users ref
+					userRef.child(result.user.uid).once("value", function(snapshot) {
+						// If there is no user with this id, make a new object with
+						// this user's id and set its name and photoURL and add it as
+						// a child to your user ref
 
-			// Check to see if we have already saved a user with this id
-			// to our users ref
+						var data = snapshot.val();
+						if (data === null) {
+							userRef.child(result.user.uid).set({
+								"name": result.user.displayName,
+								"photoURL": result.user.photoURL
+							});
+						}
 
-				// If there is no user with this id, make a new object with
-				// this user's id and set its name and photoURL and add it as
-				// a child to your user ref
-
-			// Navigate to the user.html page once sign in is complete
+					// Navigate to the user.html page once sign in is complete
+						window.location = "./views/user.html"
+					})
+			})
 	}
 
 	$("#google-signin").on("click", function() {
--- exercise-2/js/user.js
+++ exercise-2/js/user.js
@@ -1,24 +1,33 @@
 $(function() {
 	// Set the config object with the values for your application
 	var config = {
-		apiKey: "",
-		authDomain: "",
-		databaseURL: "",
-		storageBucket: "",
-		messagingSenderId: ""
+		apiKey: "AIzaSyCR1jnU6mApIaIzLUIXcxXnrVewhvSbW_E",
+		authDomain: "awesome-stuff-18c59.firebaseapp.com",
+		databaseURL: "https://awesome-stuff-18c59.firebaseio.com",
+		storageBucket: "awesome-stuff-18c59.appspot.com",
+		messagingSenderId: "731170506996"
 	};
 	firebase.initializeApp(config);
 
 	// Initialize your 'users' firebase reference
+
+	var userRef = firebase.database().ref("users");
 
 	// Get all the items in 'users' and for each item, render it
 	// (for now just call renderUser for each user object, we'll
 	// write that function next). If you run out of time to actually
 	// render the users to the screen, you can simply print them to
 	// the console.
+	users.on("value", function(snapshot) {
+		var data = snapshot.val();
+		Object.keys(data).forEach(function(d) {
+			renderUser(d, data[d]);
+		});
+	});
 
 
 	// Get the element with id cards. This is where we'll add each user card
+	var cards = $("#cards");
 
 	var renderUser = function(id, content) {
 		// Make a div for the card with classes 'card' and 'valign-wrapper'
@@ -36,12 +45,17 @@ $(function() {
 
 	// Goes back to index.html if the user is signed out
 	firebase.auth().onAuthStateChanged(function(user) {
-		if (!user) {
+		if (user) {
+			getUsers();
+		} else {
 			window.location = "../index.html";
 		}
 	});
 
 	$("#signout").on('click', function() {
 		// Sign out the current user
+		firebase.auth().signOut().then(function() {
+			window.location = "../index.html";
+		})
 	});
 });
