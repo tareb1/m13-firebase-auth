d46b90c1291a1087441f1729b384bf48421a4fe2
iGuest <tareb1@uw.edu>
2016-10-25 10:20:22 -0700

    participation

--- exercise-1/js/authenticate.js
+++ exercise-1/js/authenticate.js
@@ -1,16 +1,39 @@
 // JavaScript authentication file
 $(function() {
 
-    // Initialize Firebase
+  // Initialize Firebase
+	  var config = {
+	    apiKey: "AIzaSyCR1jnU6mApIaIzLUIXcxXnrVewhvSbW_E",
+	    authDomain: "awesome-stuff-18c59.firebaseapp.com",
+	    databaseURL: "https://awesome-stuff-18c59.firebaseio.com",
+	    storageBucket: "awesome-stuff-18c59.appspot.com",
+	    messagingSenderId: "731170506996"
+	  };
+	  firebase.initializeApp(config);
+
 
     // Sign Up: Function to create account on firebase, then redirect to index.html
     var signUp = function() {
         // Get email, password, and display name
+         var email = $('#email').val();
+         var password = $("#password").val();
+         var displayName = $('#display-name').val();
+         console.log(email, password, displayName);
 
 
         // Create user, then set the user's display name
+        	firebase.auth().createUserWithEmailAndPassword(email, password)
+        		.then(function(user){ // returns a promise
 
-                // Set display name
+                 // Set display name
+                user.updateProfile({ 
+                    displayName: displayName 
+                }).then(function() {       
+                    window.location = '/';
+                });
+            }).catch(function(error) {
+                alert(error.message);
+            });
 
     };
 
@@ -32,6 +55,18 @@ $(function() {
     };
 
     // Assign event lister to form submission
+    $('form').on('submit', function(event) {
+        event.preventDefault();
+        console.log("shjh")
+        var formId = $(this).attr('id');
+        console.log(formId)
+        if (formId == 'sign-up') {
+            signUp();
+        } else if (formId == 'sign-in') {
+            signIn();
+        }
+
+    });
 
 
 
